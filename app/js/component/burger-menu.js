b5140e833831bce8385131ebfab772a17bff45cb
Максим <makster559553@gmail.com>
2020-10-27 16:16:13 +0300

    corrected bugs with mobile menu

--- app/js/component/burger-menu.js
+++ app/js/component/burger-menu.js
@@ -1,5 +1,17 @@
 (function ($, window, document) {
 
+    function preventDefault(e) {
+        e.preventDefault();
+    }
+        
+    function disableScroll() {
+        document.body.addEventListener('touchmove', preventDefault, {passive: false});
+    }
+        
+    function enableScroll() {
+        document.body.removeEventListener('touchmove', preventDefault, {passive: false});
+    }
+
     function activateBurgerMenu(){
         if($(window).width() < 640){
             $(".js_burger-btn").on("click", function(){
@@ -8,9 +20,11 @@
                 if(!$this.hasClass("js_active")){
                     $("body").addClass("js_burger-active");
                     $this.addClass("js_active");
+                    disableScroll();
                 }else{
                     $("body").removeClass("js_burger-active");
                     $this.removeClass("js_active");
+                    enableScroll();
                 }
              })
         }
